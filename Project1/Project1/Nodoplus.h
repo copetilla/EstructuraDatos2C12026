80b61403cebee60c6461ae7ad284fb4a05f6f806
marioaje <marioaje@gmail.com>
2026-02-23 18:55:47 -0600

    nodplus

---
+++ Project1/Project1/Nodoplus.h
@@ -0,0 +1,29 @@
+//Caso práctico : Simulación de sistema de archivos con Árbol B +
+
+//Cada nodo interno representa un índice de directorio.
+
+//Cada hoja contiene las referencias a los archivos reales.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+const int ORDEN = 3; // Orden del árbol B+
+
+class Nodoplus {
+
+	public:
+
+		bool esHoja;		
+		vector<string> claves; // Vector de claves para hojas (//Cada nodo interno representa un índice de directorio.)
+		vector<Nodoplus*> hijos; // Vector de punteros a nodos hijos
+		Nodoplus* siguiente; // Puntero al siguiente nodo hoja para enlazar las hojas
+
+		Nodoplus(bool hoja) {
+			esHoja = hoja;
+			siguiente = nullptr;
+		}
+};
+
+
+
